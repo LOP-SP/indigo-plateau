1f602294ccda0ba9aa31a9733419e6c957d63662
Carlos Agarie <carlos.agarie@gmail.com>
2012-03-06 00:24:43 -0300

    Its a small plugin, better to have few files

--- indigo-plateau.php
+++ indigo-plateau.php
@@ -24,11 +24,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-define( 'INDIGO_PLATEAU_DIR', WP_PLUGIN_DIR . '/indigo-plateau' );
 
-global $ip_db_version = '1.0';
 
-// install function
 function indigo_plateau_activate () {
 	global $wpdb;
 	
@@ -44,15 +41,11 @@ function indigo_plateau_activate () {
 	  UNIQUE KEY id (id)
 	);";
 	
-	// if ( $table_name exists )
 	$wpdb->query($create_table_sql);
-	
-	add_option( 'ip_db_version', $ip_db_version );
 }
 
 register_activation_hook( __FILE__, 'indigo_plateau_activate' );
 
-// menu
 function indigo_plateau_menu () {
 	global $wpdb;
 	
@@ -70,19 +63,47 @@ function indigo_plateau_admin_actions () {
 }
 
 // Hash to store reasons
-global $reasons = array(
+global $reasons;
+$reasons = array(
 	"ganharTorneio" => 15,
 	"perderQuartas" => 10,
 	"defenderGinasio" => 5,
 	"trazerAmigo" => 15,
 	"trazerAmigo" => 5,
 	"criarPost" => 5,
-	"criarRegra" => 5,
+	"criarRegra" => 5
 );
 
 add_action( 'admin_menu', 'indigo_plateau_admin_actions' );
 
-require_once INDIGO_PLATEAU_DIR . 'input_controller.php';
-require_once INDIGO_PLATEAU_DIR . 'output_controller.php';
+function ip_insert_win ($name, $time, $event, $reason) {
+	global $wpdb, $reasons;
+	
+	$points = $reasons($reason);
+	
+	$wpdb->insert(
+		$wpdb->prefix . 'indigo_plateau',
+		array(
+			'name' => $name,
+			'time' => $time,
+			'event' => $event,
+			'reason' => $reason,
+			'points' => $points
+		),
+		array(
+			'%s',
+			'%s',
+			'%s',
+			'%s',
+			'%d'
+		)
+	);
+}
+
+function indigo_plateau_ranking () {
+	$ranking = '';
+	
+	return $ranking;
+}
 
 ?>
